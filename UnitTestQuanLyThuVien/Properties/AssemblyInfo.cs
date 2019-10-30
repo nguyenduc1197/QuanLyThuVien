e0146dc2888e0ef968d1ba6c6111f39a2d96f77f
nguyenduc1197 <57165599+nguyenduc1197@users.noreply.github.com>
2019-10-30 19:45:18 +0700

    full

---
+++ UnitTestQuanLyThuVien/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestQuanLyThuVien")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestQuanLyThuVien")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("baa74042-4039-4abc-b1d5-49ac3189fe78")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestQuanLyThuVien/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using THKiemDinh;
+using THKiemDinh.Models;
+
+namespace UnitTestQuanLyThuVien
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void Login()
+        {
+            string username = "nhanvien";
+            string password = "Account not exists";
+            string expected = "Account not exists";
+
+        
+            Assert.AreEqual(password,expected);
+        }
+    }
+}
